25cc25bee1905ccf36a65203491066d1e4c3b3cc
hhxianqu <735913885@qq.com>
2019-12-13 17:44:12 +0800

    web端框架

---
+++ SocialMediaSiteWeb/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3">消息中心</el-menu-item>
+      <el-menu-item index="4">订单管理</el-menu-item>
+    </el-menu>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '3',
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
---
+++ SocialMediaSiteWeb/src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <el-input placeholder="请输入用户名" prefix-icon="el-icon-user" v-model="username"></el-input>
+        <el-input placeholder="请输入密码" prefix-icon="el-icon-lock" v-model="password" show-password></el-input>
+
+        <el-button type="primary">登陆</el-button>
+        <el-button type="primary">注册</el-button>
+    </div>
+</template>
+
+<script>
+import { Vue } from 'vue-property-decorator';
+
+export default {
+  name: 'login',
+  components: {
+
+  },
+  data: {
+      username: '',
+      password: '',
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
